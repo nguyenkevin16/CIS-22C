50f818df940df2529ca1b139f061bb4fa6333c67
Kevin D. Nguyen <nguyenkevin16@users.noreply.github.com>
2016-05-19 19:17:24 -0700

    Moved RB_stats_2015 to program_doc

--- program_docs/program_docs/main.cpp
+++ program_docs/program_docs/main.cpp
@@ -7,10 +7,30 @@
 */
 
 #include <iostream>
+#include <fstream>
+#include <string>
+#include "player.h"
 using namespace std;
+
+const char FILE_INPUT[] = "RB_stats_2015.txt";
+
+// Function Prototypes
+void checkFileOpen(ifstream&, const char[]);
 
 int main() {
 
+	ifstream fin(FILE_INPUT);
+	checkFileOpen(fin, FILE_INPUT);
+
+	fin.close();
+
 	system("pause");
 	return 0;
+}
+
+void checkFileOpen(ifstream& fin, const char name[]) {
+	if (fin.fail()) {
+		cerr << "The input file " << name << " could not be found. This program will now terminate." << endl;
+		exit(1);
+	}
 }
